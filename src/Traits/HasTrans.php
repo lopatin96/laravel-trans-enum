943082cff4969f2eba2b84bee4d70755fe563abb
vladyslavlopatin <ilopatin3@gmail.com>
2023-11-07 23:45:30 +0100

    getKey; snake

--- src/Traits/HasTrans.php
+++ src/Traits/HasTrans.php
@@ -2,10 +2,17 @@
 
 namespace Atin\LaravelTransEnum\Traits;
 
+use function Symfony\Component\String\u;
+
 trait HasTrans
 {
     public function trans($replace = [], $locale = null): ?string
     {
-        return trans('enums/' . class_basename(__CLASS__) . '.' . $this->value, $replace, $locale);
+        return trans($this->getKey(), $replace, $locale);
+    }
+
+    public function getKey(): ?string
+    {
+        return 'enums/' . u(class_basename(__CLASS__))->snake()->toString() . '.' . $this->value;
     }
 }
